de8ccdb8dc2afbe9e96b6bb8ae1910a7527770ca
Isaac-blop <isaac.martirena@estudiantes.utec.edu.uy>
2025-10-04 23:06:12 -0300

    Update main.c

--- Continua/Ev 14 - HCSR04 - Tarea grupal/2 Teclado/2 Teclado/main.c
+++ Continua/Ev 14 - HCSR04 - Tarea grupal/2 Teclado/2 Teclado/main.c
@@ -1,50 +1,53 @@
-/*
- * main.c
- *
- * Created: 10/3/2025 6:13:36 PM
- *  Author: isacm
- */ 
-
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <stdint.h>
 #include <util/delay.h>
 
-#define a 0b00110010
-#define b 0b00101000
-#define c 0b00001100
-#define d 0b00000001  
+#define a 0b00110010   // Function set nibble
+#define b 0b00101000   // 4-bit mode, 2-line, 5x8 font
+#define c 0b00001100   // Display ON, cursor OFF
+#define d 0b00000001   // Clear display
+
 #define LCD_RS PB5
 #define LCD_E  PB4
 
 int LO_NIBBLE(int x) {
-	return (int)((unsigned)x & 0x0F);
+	return (int)(x & 0x0F);
 }
 
 int HI_NIBBLE(int x) {
-	return (int)(((unsigned)x >> 4) & 0x0F);
+	return (int)((x >> 4) & 0x0F);
 }
 
-void setearLCD(int x){
-	
-	PORTB &= ~(1 << LCD_RS); // para cargar instrucciones
-	
-	
-	PORTB = (PORTB & 0xF0) | LO_NIBBLE(x);
-	// Pulso a E
-	PORTB |=  (1 << LCD_E);
+void lcd_pulse(void) {
+	PORTB |= (1 << LCD_E);
 	_delay_us(1);
 	PORTB &= ~(1 << LCD_E);
-	
-	PORTB = (PORTB & 0xF0) | HI_NIBBLE(x);
-	
-	// Pulso a E
-	PORTB |=  (1 << LCD_E);
-	_delay_us(1);
-	PORTB &= ~(1 << LCD_E);
-	
-	
+	_delay_us(100);
 }
+
+void setearLCD(int x) {
+	PORTB &= ~(1 << LCD_RS);  // RS=0 → comando
+
+	PORTB = (PORTB & 0xF0) | HI_NIBBLE(x);
+	lcd_pulse();
+
+	PORTB = (PORTB & 0xF0) | LO_NIBBLE(x);
+	lcd_pulse();
+
+	if (x == d) _delay_ms(2); 
+}
+
+void Escribir_LCD(int x) {
+	PORTB |= (1 << LCD_RS);   // RS=1 → datos
+
+	PORTB = (PORTB & 0xF0) | HI_NIBBLE(x);
+	lcd_pulse();
+
+	PORTB = (PORTB & 0xF0) | LO_NIBBLE(x);
+	lcd_pulse();
+}
+
 char keypad[4][4] = {
 	{ '1', '2', '3', 'A' },
 	{ '4', '5', '6', 'B' },
@@ -52,21 +55,16 @@ char keypad[4][4] = {
 	{ '*', '0', '#', 'D' }
 };
 
-
-int main(void)
-{
-	
-	DDRB |= 0b00111111;
+int main(void) {
+	DDRB |= 0b00111111;  // PB0–PB5 como salida
 	PORTB &= ~(1 << LCD_RS);
-	
-	
+	_delay_ms(50);       // esperar a que arranque la LCD
+
 	setearLCD(a);
 	setearLCD(b);
 	setearLCD(c);
 	setearLCD(d);
-	
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+
+	Escribir_LCD('A');   // prueba con letra visible
+	while (1);
 }
