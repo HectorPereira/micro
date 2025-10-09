7fb19f2395e1a2a0b539e95fef64ea8cb74c85ad
Hector <hector.pereira@estudiantes.utec.edu.uy>
2025-10-09 18:16:01 -0300

    Basic usart

--- Laboratorios/Lab2/code/2 Colores/2 Colores/main.c
+++ Laboratorios/Lab2/code/2 Colores/2 Colores/main.c
@@ -2,15 +2,21 @@
 // LIBRARIES
 // ------------------------------------------------------------------
 
-
-#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 // -----------------------------------------------------------------
-// DEFINITIONS-
+// DEFINITIONS
 // ------------------------------------------------------------------
 
+#define F_CPU 16000000UL
+
+// USART
+#define TX_BUF_SZ 128
+#define TX_MASK   (TX_BUF_SZ - 1)
+#define RX_BUF_SZ 128
+#define RX_MASK   (RX_BUF_SZ - 1)
+#define BAUD_RATE 9600
 
 
 
@@ -18,7 +24,7 @@
 // PROGRAM VARIABLES
 // ------------------------------------------------------------------
 
-// Color recognition
+// Color mapping
 typedef struct {
 	const char *name;
 	uint16_t r, g, b;
@@ -35,24 +41,79 @@ const ColorRef color_refs[] = {
 };
 
 
+// USART
+uint8_t tx_buf[TX_BUF_SZ];
+uint8_t tx_head = 0, tx_tail = 0;
+
+uint8_t rx_buf[RX_BUF_SZ];
+uint8_t rx_head = 0, rx_tail = 0;
+
 // ------------------------------------------------------------------
 // HELPERS
 // ------------------------------------------------------------------
 
-
-
+// Retorna la cantidad de elementos en el buffer de RX
+uint8_t usart_rx_available(void) {
+	return (uint8_t)((rx_head - rx_tail) & RX_MASK);
+}
 
 // ------------------------------------------------------------------
 // INITIALIZERS
 // ------------------------------------------------------------------
 
-
+void usart_init(void) {
+	const uint16_t ubrr = (16000000UL / (16UL * BAUD_RATE)) - 1;
+	UBRR0H = ubrr >> 8;
+	UBRR0L = ubrr;
+	UCSR0A = 0;
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);   // <- RX interrupt
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);               // 8N1
+}
 
 
 // ------------------------------------------------------------------
 // UTILITY
 // ------------------------------------------------------------------
 
+
+
+uint8_t usart_write_try(uint8_t b) {
+	uint8_t next = (uint8_t)((tx_head + 1) & TX_MASK);
+	if (next == tx_tail) return 0;               // full
+	tx_buf[tx_head] = b;
+	tx_head = next;
+	UCSR0B |= (1 << UDRIE0);                       // kick the ISR
+	return 1;
+}
+
+uint16_t usart_write_str(const char *s) {
+	uint16_t n = 0;
+	while (*s && usart_write_try((uint8_t)*s++)) n++;
+	return n;
+}
+
+uint8_t usart_read_try(uint8_t *b) {
+	if (rx_head == rx_tail) return 0;                 // empty
+	*b = rx_buf[rx_tail];
+	rx_tail = (uint8_t)((rx_tail + 1) & RX_MASK);
+	return 1;
+}
+
+// Stores string in 'dest'
+uint8_t usart_read_str(char *dest, uint8_t max_len) {
+	uint8_t count = 0;
+	while (usart_rx_available() && count < (max_len - 1)) {
+		uint8_t c;
+		usart_read_try(&c);
+		if (c == '\n' || c == '\r') {
+			break;
+		}
+		dest[count++] = c;
+	}
+	
+	dest[count] = '\0';
+	return count;
+}
 
 
 
@@ -62,8 +123,10 @@ const ColorRef color_refs[] = {
 
 
 int main(void) {
+	usart_init();
+	sei();
 	while (1) {
-
+	
 	}
 }
 
@@ -71,3 +134,23 @@ int main(void) {
 // ISRs
 // ------------------------------------------------------------------
 
+ISR(USART_UDRE_vect) {
+	if (tx_head == tx_tail) {
+		UCSR0B &= (uint8_t)~_BV(UDRIE0);
+		return;
+	}
+	UDR0 = tx_buf[tx_tail];
+	tx_tail = (uint8_t)((tx_tail + 1) & TX_MASK);
+}
+
+ISR(USART_RX_vect) {
+	uint8_t d = UDR0;
+	uint8_t next = (uint8_t)((rx_head + 1) & RX_MASK);
+	if (next != rx_tail) {
+		rx_buf[rx_head] = d;
+		rx_head = next;
+	}
+	
+	// uint8_t data = usart_read_try();
+	// ... check for command character
+}
