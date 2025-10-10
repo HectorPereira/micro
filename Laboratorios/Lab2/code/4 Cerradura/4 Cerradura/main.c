a512de9126727a672e9bea3807c4706f767d6aeb
Hector <hector.pereira@estudiantes.utec.edu.uy>
2025-10-10 14:57:21 -0300

    Consigna cerradura

--- Laboratorios/Lab2/code/4 Cerradura/4 Cerradura/main.c
+++ Laboratorios/Lab2/code/4 Cerradura/4 Cerradura/main.c
@@ -1,8 +1,30 @@
 /*
- * main.c
- *
- * Created: 10/1/2025 2:20:22 PM
- *  Author: hecto
+Funcionalidades detalladas:
+
+1. Bienvenida y Menú Interactivo:
+	-	Al encender el sistema, la pantalla LCD muestra un mensaje de
+		bienvenida e instrucciones para ingresar la contraseña.
+	-	El menú también da la opción de cambiar la contraseña si el
+		usuario lo desea.
+
+2. Ingreso de Contraseña:
+	-	El usuario ingresa la contraseña mediante el teclado matricial.
+	-	La contraseña es comparada con la almacenada en la EEPROM.
+	-	Si es correcta, se enciende el LED verde.
+	-	Si es incorrecta, se enciende el LED rojo. Después de 3 intentos
+		fallidos, se activa la alarma (buzzer).
+
+3. Almacenamiento en EEPROM:
+	-	La contraseña se guarda en la EEPROM para que esté disponible
+		después de un reinicio o apagado del sistema.
+	-	El microcontrolador puede leer la contraseña desde la EEPROM y
+		comparar los valores con lo que el usuario ingresa.
+
+4. Cambio de Contraseña:
+	-	El sistema permite al usuario cambiar la contraseña ingresando
+		primero la contraseña actual.
+	-	Luego, el usuario puede elegir una nueva contraseña de entre 4 y
+		6 dígitos, que será almacenada en la EEPROM.
  */ 
 
 #include <xc.h>
