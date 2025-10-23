744b4ec7527112bf111d2ce6984d29af481f9c8a
Hector <hector.pereira@estudiantes.utec.edu.uy>
2025-10-23 11:41:42 -0300

    created projects

---
+++ Laboratorios/Lab3/code/1 Plotter/Plotter/Plotter/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 10/23/2025 11:35:05 AM
+ *  Author: hecto
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ Laboratorios/Lab3/code/4 Matriz/Matriz/Matriz/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 10/23/2025 11:38:04 AM
+ *  Author: hecto
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
