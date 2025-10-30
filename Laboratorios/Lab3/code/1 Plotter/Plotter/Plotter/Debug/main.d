c8a614a5263ebd7d0cdabdb0ecb527284eea2052
Hector <hector.pereira@estudiantes.utec.edu.uy>
2025-10-30 12:57:59 -0300

    plotter final

--- Laboratorios/Lab3/code/1 Plotter/Plotter/Plotter/Debug/main.d
+++ Laboratorios/Lab3/code/1 Plotter/Plotter/Plotter/Debug/main.d
@@ -19,7 +19,7 @@ main.d main.o: .././main.c \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\interrupt.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\pgmspace.h \
  c:\program\ files\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stddef.h \
- .././murcielago_path.h .././circle_data_path.h .././flor_path.h
+ .././path_data.h
 
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\io.h:
 
@@ -61,8 +61,4 @@ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\pgmspace.h:
 
 c:\program\ files\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stddef.h:
 
-.././murcielago_path.h:
-
-.././circle_data_path.h:
-
-.././flor_path.h:
+.././path_data.h:
