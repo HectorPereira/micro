f91201672264f75d4b2adc8bae8d4c91a6f00078
Isaac-blop <isaac.martirena@estudiantes.utec.edu.uy>
2025-11-07 11:07:29 -0300

    Aregue proyectos

---
+++ Laboratorios/Lab4/code/2 I2C/Comunicacion I2C/Comunicacion I2C/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/7/2025 11:04:50 AM
+ *  Author: isacm
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
