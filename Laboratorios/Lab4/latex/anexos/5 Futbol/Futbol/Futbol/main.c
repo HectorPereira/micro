7b9e3056d159deb7a31ef398f3e6f1e0ea554ef0
Hector <hector.pereira@estudiantes.utec.edu.uy>
2025-11-06 22:12:39 -0300

    Limpieza laboratorio 4

---
+++ Laboratorios/Lab4/latex/anexos/5 Futbol/Futbol/Futbol/main.c
@@ -0,0 +1,107 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#define mp 10000
+#define Pin_M PORTB2
+
+double dutyCycle = 0;
+
+
+
+
+void uart_init(void) {
+	// 9600 baud @ 16 MHz
+	uint16_t ubrr = 103;  // from formula: UBRR = (F_CPU / (16 * BAUD)) - 1
+	UBRR0H = (uint8_t)(ubrr >> 8);
+	UBRR0L = (uint8_t)(ubrr);
+
+	// Enable transmitter and receiver
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+
+	// Frame format: 8 data bits, 1 stop bit, no parity
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void uart_tx(char c) {
+	// Wait for empty transmit buffer
+	while (!(UCSR0A & (1 << UDRE0)));
+	UDR0 = c;
+}
+
+char uart_rx(void) {
+	// Wait for data to be received
+	while (!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
+void motor_init(void){
+	DDRD |= (1<<PORTD6) | (1<<PORTD7);
+}
+
+
+//PWM para el control del motor paso a paso 
+void init_motor2(){
+	DDRB |= (1<<Pin_M);
+	
+	TCCR0A |= (1 << WGM11) | (1<<COM1A1);
+	TCCR0B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11); // clk/8
+	ICR1 = 39999;
+	
+	OCR0A = (dutyCycle/100)*255;
+}
+
+void left(){
+	PORTD |= (1<<PORTD6);
+	PORTD &= ~(1<<PORTD7);
+}
+
+void right(){
+	PORTD |= (1<<PORTD7);
+	PORTD &= ~(1<<PORTD6);
+}
+
+void forward(){
+	PORTD |= (1<<PORTD7);
+	PORTD |= (1<<PORTD6);
+}
+
+void stop(){
+	PORTD &= ~(1<<PORTD7);
+	PORTD &= ~(1<<PORTD6);
+}
+
+void punch(){
+	for(uint8_t i = 0; i < 100; i++){
+		OCR1A +=20;
+	}
+}
+
+void uart_println(char c) {
+	uart_tx(c);
+	uart_tx('\r');
+	uart_tx('\n');
+}
+
+
+
+int main(void) {
+	uart_init();
+	//motor_init();
+	init_motor2();
+	
+	while (1) {
+		punch();
+		// If data available (RX complete flag)
+		if (UCSR0A & (1 << RXC0)) {
+			char val = uart_rx();
+			uart_println(val);
+			if (val == 'F') forward();
+			if (val == 'L') left();
+			if (val == 'R') right();
+			if (val == 'P') punch();
+			if (val == '0') stop();
+		}
+	}
+}
