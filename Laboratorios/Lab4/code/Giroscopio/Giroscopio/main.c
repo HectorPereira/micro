ec10508a6ea2284c5c41cb6f4e03f6beaf808bb3
Hector <hector.pereira@estudiantes.utec.edu.uy>
2025-11-07 16:42:37 -0300

    proyectos creados

---
+++ Laboratorios/Lab4/code/Giroscopio/Giroscopio/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/7/2025 4:41:57 PM
+ *  Author: hecto
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ Laboratorios/Lab4/code/I2C/I2C/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/7/2025 4:40:56 PM
+ *  Author: hecto
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
