2f4eeb6f987e3fe6ad42876959d344be632a9789
Isaac-blop <isaac.martirena@estudiantes.utec.edu.uy>
2025-11-13 21:40:11 -0300

    I2c - 1

---
+++ Laboratorios/Lab4/code/2 I2C/I2C slave/I2C slave/I2C - Esclavo/I2C - Esclavo/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 11/13/2025 7:52:44 PM
+ *  Author: isacm
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
